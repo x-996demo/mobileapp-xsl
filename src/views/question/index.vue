6656df459f2469fc66c8ddc136632f773ddbb30c
xsl <x976403038@163.com>
2020-03-15 18:39:34 +0800

    调整结构,项目雏形

---
+++ src/views/question/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+      问答模块
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/views/search/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+      搜索模块
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
