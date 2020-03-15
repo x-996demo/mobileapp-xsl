15047c3e9e47311bb8bad5ebe211d73bc83082e1
xsl <x976403038@163.com>
2020-03-15 19:05:41 +0800

    适配rem

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import router from './router'
 import store from './store'
 import Vant from 'vant' // 引入vant组件
 import 'vant/lib/index.css' // 引入vant组件样式
+import 'amfe-flexible'
 Vue.config.productionTip = false
 Vue.use(Vant) // 全局注册  实际上调用了 Vant导出的对象的方法  install方法
 new Vue({
