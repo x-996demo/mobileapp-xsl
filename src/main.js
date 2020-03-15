61af51ef6a4d3e58b04f9d165b27714795e35b17
xsl <x976403038@163.com>
2020-03-15 18:54:39 +0800

    引入Vant

--- src/main.js
+++ src/main.js
@@ -2,9 +2,10 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-
+import Vant from 'vant' // 引入vant组件
+import 'vant/lib/index.css' // 引入vant组件样式
 Vue.config.productionTip = false
-
+Vue.use(Vant) // 全局注册  实际上调用了 Vant导出的对象的方法  install方法
 new Vue({
   router,
   store,
