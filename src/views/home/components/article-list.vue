4d00202da3fc3f23f1464ba3b9efe9d9e8108880
xsl <x976403038@163.com>
2020-03-19 22:25:15 +0800

    article-列表文章布局

--- src/views/home/components/article-list.vue
+++ src/views/home/components/article-list.vue
@@ -1,23 +1,70 @@
 <template>
-   <!-- 文章列表组件  放置列表 -->
+  <!-- 文章列表组件  放置列表 -->
   <!-- van-list 可以帮助我们实现上拉加载  需要一些变量 -->
   <!-- 这里放置这个div的目的是 形成滚动条, 因为我们后期要做 阅读记忆 -->
   <!-- 阅读记忆  上次你阅读到哪  回来之后还是哪-->
   <div class="scroll-wrapper">
-      <!-- 文章列表 -->
-      <!-- van-list组件 如果不加干涉, 初始化完毕 就会检测 自己距离底部的长度,如果超过了限定 ,就会执行 load事件  自动把
+    <!-- 文章列表 -->
+    <!-- van-list组件 如果不加干涉, 初始化完毕 就会检测 自己距离底部的长度,如果超过了限定 ,就会执行 load事件  自动把
        绑定的 loading 变成true
-      -->
-       <!-- 下拉刷新组件包裹 列表组件 -->
+    -->
+    <!-- 下拉刷新组件包裹 列表组件 -->
     <van-pull-refresh v-model="downLoading" @refresh="onRefresh" :success-text="successText">
-    <van-list finished-text='没有了' v-model="upLoading" :finished="finished" @load="onLoad">
-      <!-- 循环内容 -->
-      <van-cell-group>
-        <van-cell  v-for="item in articles" :key="item" title="美股又熔断了" :value="'天台排队'+item"></van-cell>
-      </van-cell-group>
-
-    </van-list>
-     </van-pull-refresh>
+      <van-list finished-text="没有了" v-model="upLoading" :finished="finished" @load="onLoad">
+        <!-- 循环内容 -->
+        <van-cell-group>
+          <van-cell v-for="item in articles" :key="item">
+            <!-- 放置元素 文章列表的循环项 无图 单图 三图 -->
+            <div class="article_item">
+              <h3 class="van-ellipsis">哈哈哈,前端真的是太难了</h3>
+              <div class="img_box">
+                <van-image class="w33" fit="cover" src="https://img.yzcdn.cn/vant/cat.jpeg" />
+                <van-image class="w33" fit="cover" src="https://img.yzcdn.cn/vant/cat.jpeg" />
+                <van-image class="w33" fit="cover" src="https://img.yzcdn.cn/vant/cat.jpeg" />
+              </div>
+              <div class="info_box">
+                <span>你像一阵风</span>
+                <span>8评论</span>
+                <span>10分钟前</span>
+                <span class="close">
+                  <van-icon name="cross"></van-icon>
+                </span>
+              </div>
+            </div>
+            <!-- 单图 -->
+            <div class="article_item">
+              <h3 class="van-ellipsis">PullRefresh下拉刷新PullRefresh下拉刷新下拉刷新下拉刷新</h3>
+              <div class="img_box">
+                <van-image class="w100" fit="cover" src="https://img.yzcdn.cn/vant/cat.jpeg" />
+              </div>
+              <div class="info_box">
+                <span>你像一阵风</span>
+                <span>8评论</span>
+                <span>10分钟前</span>
+                <span class="close">
+                  <van-icon name="cross"></van-icon>
+                </span>
+              </div>
+            </div>
+            <!-- 无图 -->
+            <div class="article_item">
+              <h3 class="van-ellipsis">PullRefresh下拉刷新PullRefresh下拉刷新下拉刷新下拉刷新</h3>
+              <!-- <div class="img_box">
+                <van-image class="w100" fit="cover" src="https://img.yzcdn.cn/vant/cat.jpeg" />
+              </div> -->
+              <div class="info_box">
+                <span>你像一阵风</span>
+                <span>8评论</span>
+                <span>10分钟前</span>
+                <span class="close">
+                  <van-icon name="cross"></van-icon>
+                </span>
+              </div>
+            </div>
+          </van-cell>
+        </van-cell-group>
+      </van-list>
+    </van-pull-refresh>
   </div>
 </template>
 
@@ -42,7 +89,10 @@ export default {
         // 如果此时记录大于100
         this.finished = true
       } else {
-        const arr = Array.from(Array(15), (value, index) => this.articles.length + index + 1)
+        const arr = Array.from(
+          Array(15),
+          (value, index) => this.articles.length + index + 1
+        )
         // 上拉加载 不是覆盖之前的数据 应该把 数据追加到数组的队尾
         this.articles.push(...arr)
         // 加载完毕 需要手动关掉loading
@@ -58,8 +108,11 @@ export default {
     },
     onRefresh () {
       setTimeout(() => {
-      // 下拉刷新 表示要读取最新的数据 而且最新的数据要添加到数据头部
-        const arr = Array.from(Array(2), (value, index) => '追加' + (index + 1))
+        // 下拉刷新 表示要读取最新的数据 而且最新的数据要添加到数据头部
+        const arr = Array.from(
+          Array(2),
+          (value, index) => '追加' + (index + 1)
+        )
         // 数组添加到头部
         this.articles.unshift(...arr)
         // 手动关闭正在加载的状态
@@ -68,10 +121,48 @@ export default {
       }, 1000)
     }
   }
-
 }
 </script>
 
-<style>
-
+<style lang='less' scoped>
+.article_item {
+  h3 {
+    font-weight: normal;
+    line-height: 2;
+  }
+  .img_box {
+    display: flex;
+    justify-content: space-between;
+    .w33 {
+      width: 33%;
+      height: 90px;
+    }
+    .w100 {
+      width: 100%;
+      height: 180px;
+    }
+  }
+  .info_box {
+    color: #999;
+    line-height: 2;
+    position: relative;
+    font-size: 12px;
+    span {
+      padding-right: 10px;
+      &.close {
+        border: 1px solid #ddd;
+        border-radius: 2px;
+        line-height: 15px;
+        height: 12px;
+        width: 16px;
+        text-align: center;
+        padding-right: 0;
+        font-size: 8px;
+        position: absolute;
+        right: 0;
+        top: 7px;
+      }
+    }
+  }
+}
 </style>
